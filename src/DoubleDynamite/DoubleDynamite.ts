fc17f6d13dc016ce605f12162f95960778d4fcdc
Denoflions <denvstheworld@gmail.com>
2021-10-11 09:13:35 -0400

    update for Z64O

--- src/DoubleDynamite/DoubleDynamite.ts
+++ src/DoubleDynamite/DoubleDynamite.ts
@@ -1,42 +1,9 @@
-import { IPlugin, IModLoaderAPI } from 'modloader64_api/IModLoaderAPI';
-import { IOOTCore, OotEvents } from 'modloader64_api/OOT/OOTAPI';
-import { InjectCore } from 'modloader64_api/CoreInjection';
-import { EventHandler } from 'modloader64_api/EventHandler';
-import { IActor } from 'modloader64_api/OOT/IActor';
+import { ProxySide, SidedProxy } from 'modloader64_api/SidedProxy/SidedProxy';
+import DoubleDynamiteClient from './DoubleDynamiteClient';
 
-class DoubleDynamite implements IPlugin {
+export default class DoubleDynamite{
 
-    ModLoader!: IModLoaderAPI;
-    pluginName?: string | undefined;
-    @InjectCore()
-    core!: IOOTCore;
-    phaseSkipped: boolean = false;
-
-    preinit(): void {
-    }
-    init(): void {
-    }
-    postinit(): void {
-    }
-    onTick(frame?: number | undefined): void {
-    }
-
-    @EventHandler(OotEvents.ON_ACTOR_SPAWN)
-    onAnnoyingBossSpawned(actor: IActor) {
-        if (actor.actorID === 0x00DC && !this.phaseSkipped) {
-            if (actor.health === 0) {
-                actor.health = 0x4;
-                this.phaseSkipped = true;
-                this.ModLoader.logger.info("Skipping Twinrova phase 1.");
-            }
-        }
-    }
-
-    @EventHandler(OotEvents.ON_LOADING_ZONE)
-    onSceneChanged(evt: any) {
-        this.phaseSkipped = false;
-    }
+    @SidedProxy(ProxySide.CLIENT, DoubleDynamiteClient)
+    client!: DoubleDynamiteClient;
 
 }
-
-module.exports = DoubleDynamite;
---
+++ src/DoubleDynamite/DoubleDynamiteClient.ts
@@ -0,0 +1,32 @@
+import { IModLoaderAPI } from 'modloader64_api/IModLoaderAPI';
+import { InjectCore } from 'modloader64_api/CoreInjection';
+import { EventHandler } from 'modloader64_api/EventHandler';
+import { IZ64Main } from 'Z64Lib/API/Common/IZ64Main';
+import { IActor, Z64 } from 'Z64Lib/API/imports';
+import { ModLoaderAPIInject } from 'modloader64_api/ModLoaderAPIInjector';
+
+export default class DoubleDynamiteClient {
+
+    @ModLoaderAPIInject()
+    ModLoader!: IModLoaderAPI;
+    @InjectCore()
+    core!: IZ64Main;
+    phaseSkipped: boolean = false;
+
+    @EventHandler(Z64.OotEvents.ON_ACTOR_SPAWN)
+    onAnnoyingBossSpawned(actor: IActor) {
+        if (actor.actorID === 0x00DC && !this.phaseSkipped) {
+            if (actor.health === 0) {
+                actor.health = 0x4;
+                this.phaseSkipped = true;
+                this.ModLoader.logger.info("Skipping Twinrova phase 1.");
+            }
+        }
+    }
+
+    @EventHandler(Z64.OotEvents.ON_LOADING_ZONE)
+    onSceneChanged(evt: any) {
+        this.phaseSkipped = false;
+    }
+
+}
